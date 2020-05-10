9f8143545e230961c6464c3743cf3659f66436e8
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2020-05-10 18:24:37 +0300

    add MenuItem price field format validation

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,8 +3,9 @@ class MenuItem < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :orders
   has_many :item_histories, autosave: true, :dependent => :destroy
-
   has_one_attached :photo
+
+  validates_numericality_of :price, :greater_than => 0.0
 
   scope :lazy_attach_and_ordered_by_category, -> { with_attached_photo.order("category_id asc") }
 end
