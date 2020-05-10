45ff9d0380e6ecf467acbb1f4f62ad0c9e7afdd2
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2020-05-10 13:50:07 +0300

    fix spec User cannot create MenuItems

--- spec/requests/menu_items_request_spec.rb
+++ spec/requests/menu_items_request_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe "MenuItems", type: :request do
 
   describe "GET /create" do
-    it "returns http success" do
-      get "/menu_items/create"
-      expect(response).to have_http_status(:success)
+
+    context "non-admin not authorized"
+      it "returns http found" do
+        user = FactoryBot.create(:user)
+        login_as(user, :scope => :user)
+        get "/admin/menu_items/create"
+        expect(response).to have_http_status(:found)
     end
   end
 
