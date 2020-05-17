6175fac758f30d0482c4a4258dec244a4aa5217a
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2020-05-17 21:14:57 +0300

    change Orders bd request method to reduce number of Categories requests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,13 +9,15 @@ class OrdersController < WebApplicationController
     if current_user.try(:admin)
         @order_access = "admin"
         if @requested_day
-          @orders = Order.where(created_at: @requested_day.midnight..@requested_day.end_of_day).order("id desc")
+          @orders = Order.where(created_at: @requested_day.midnight..@requested_day.end_of_day)
+                        .order("id desc")
+                        .includes(menu_items: :category)
         else
-          @orders = Order.all.order("id desc")
+          @orders = Order.all.order("id desc").includes(menu_items: :category)
         end
     elsif user_signed_in?
         @order_access = "customer"
-        @orders = Order.for_current_customer(current_user.id)
+        @orders = Order.for_current_customer(current_user.id).includes(menu_items: :category)
     end
   end
 end
